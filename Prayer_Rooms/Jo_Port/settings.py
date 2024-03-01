0fc0bf957e3e4658e4f60d0f57fe75f04b2b4c32
JoanWaithira <joanwaithira.jw@gmail.com>
2024-03-01 14:16:00 +0300

    Debug = False

--- Prayer_Rooms/Jo_Port/settings.py
+++ Prayer_Rooms/Jo_Port/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m3#^%kgtqz42tf%5014r_-7pwk$ob$@-z&8_tz(@=loh_3=1#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
